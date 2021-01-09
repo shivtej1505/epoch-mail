8105bbc06ff53a701850c9f7317020f1e2ab3eb5
Shivang Nagaria <shivang.nagaria@gmail.com>
2021-01-09 14:26:51 +0530

    Associated setup done

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,2 +1,4 @@
 class Box < ApplicationRecord
+  has_many :box_mails
+  has_many :inbound_mails, through: :box_mails
 end
--- app/models/box_mail.rb
+++ app/models/box_mail.rb
@@ -1,4 +1,4 @@
 class BoxMail < ApplicationRecord
   belongs_to :box
-  belongs_to :inbound_mail
+  belongs_to :inbound_mail, class_name: "ActionMailbox::InboundEmail"
 end
