685016ab0ec774c0841fd2e2921f8823f6186f28
Shivang Nagaria <shivang.nagaria@gmail.com>
2021-01-09 19:21:46 +0530

    Added mail receiving stub

--- app/mailboxes/inbox_mailbox.rb
+++ app/mailboxes/inbox_mailbox.rb
@@ -1,5 +1,9 @@
 class InboxMailbox < ApplicationMailbox
   def process
-    puts mail
+    to_list = mail.to
+    Box.where(email: to_list).each do |box|
+      box.box_mails << BoxMail.new(inbound_mail_id: @inbound_email.id)
+      box.save!
+    end
   end
 end
